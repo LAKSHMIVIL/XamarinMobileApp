e5d5b1e795e0f60131fa9da66da6cc3ee41aa783
LAKSHMIVIL <96649159+LAKSHMIVIL@users.noreply.github.com>
2022-04-29 21:35:46 +0530

    XamarinMobileApp

---
+++ XamarinMobileApp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        bool m_BtnClicked;
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        private void DashMyPolicies_Click(object sender, EventArgs e)
+        {
+
+            if (!m_BtnClicked)
+            {
+                m_BtnClicked = true;
+                Navigation.PushAsync(new MyPolicies());
+
+            }
+            else
+            {
+                m_BtnClicked = false;
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            m_BtnClicked = false;
+        }
+    }
+
+}
